7a6933b2759355316c06c80185803f0a4bf3afa5
infopz <casari.giovanni@gmail.com>
2017-08-20 21:25:52 +0200

    remove cose

--- test_database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as lite
-
-con = lite.connect('diary.db')
-with con:
-    #con.row_factory = lite.Row
-    cur = con.cursor()
-    cur.execute("SELECT AVG(Vote), Subject, Type FROM Votes GROUP BY Subject, Type")
-    rows = cur.fetchall()
-    print(rows)
